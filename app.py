dcc70106fd7fcb404ba76dd9ffbdd997a494e27d
Victor Contreras <victor.contreras.c@uni.pe>
2022-05-29 18:46:34 -0500

    si algo no está roto, no lo arregles

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1> Cara con sombrero de vaquero 🤠 </h1> <p>Flask en Heroku gaaa</p>'
+    return render_template('index.html')
 
 if __name__=='__main__':
-    app.run()
+    app.run(debug=True, port=5000)
